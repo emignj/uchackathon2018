c47b3f4d7eaaa72c34e2078e73ba4b14920447fc
Mailys Terrier <terriems@mail.uc.edu>
2018-03-03 17:23:07 -0500

    Copied eJournal HTML and CS

---
+++ design.cs
@@ -0,0 +1,101 @@
+.header {
+  color: #000000;
+  font-family: 'Forte';
+  font-size: 40px;
+  font-weight: 500;
+  line-height: 20px;
+  text-align: center;
+}
+
+.page-header {
+    background-color: #cce6ff
+}
+
+.title h3 a {
+  color: #000000;
+  font-family: 'Bookman Old Style';
+  font-size: 30px;
+  font-style: normal;
+  font-weight:600;
+}
+
+
+.title {
+  color: #000000;
+  font-family: 'Bookman Old Style';
+  font-size: 30px;
+  font-style: normal;
+  font-weight:600;
+}
+
+.post_body {
+  padding-left: 30px;
+  color: #000000;
+  font-size: 18px;
+  font-style: normal;
+  font-weight: 300;
+  font-family: "Calibri";
+}
+
+.date {
+  padding-left: 30px;
+  color: #000000;
+  font-size: 15px;
+  font-family: "Calibri"
+}
+
+.save {
+  float: right;
+}
+
+.post-form textarea, .post-form input {
+  width: 100%;
+}
+/* not yet sure what this does! */
+
+.btn {
+  float: right;
+}
+
+
+/* STUFF FOR THE CALENDAR !!!! */
+
+#calendar table {
+    width: 100%;
+}
+
+#calendar table tr th {
+    text-align: center;
+    font-size: 16px;
+    background-color: #316497;
+    color: #99ccff;
+}
+
+#calendar table tr td {
+    width: 10%;
+    border: 1px solid #555;
+    vertical-align: top;
+    height: 120px;
+    padding: 2px;
+}
+
+#calendar td.noday {
+    background-color: #eee;
+}
+
+#calendar td.filled {
+    background-color: #99ccff;
+}
+
+#calendar td.today {
+    border: 4px solid #316497;
+}
+
+#calendar .dayNumber {
+    font-size: 16px !important;
+    font-weight: bold;
+}
+
+#calendar a {
+    font-size: 10px;
+}
