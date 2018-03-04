d24b5c7ad710db6386ddae55b3db90e0d29e5953
patel3mh <patel3mh@mail.uc.edu>
2018-03-04 08:10:56 -0500

    fixed merge conflicts

--- myScript.js
+++ myScript.js
@@ -1,12 +1,11 @@
 var items = new Array();
-var table = document.getElementById("itemList")
-var buttonAdd = document.querySelector(".addItem")
+var buttonAdd = document.querySelector("#addItem")
 
 
 buttonAdd.onclick = function() {
-    var name = document.querySelector(".itemname")
+    var name = document.querySelector("#itemname")
     var _itemName = name.value;
-    var expiration = document.querySelector(".expiration")
+    var expiration = document.querySelector("#expiredate")
     var _itemExpiration = expiration.value;
     var today = new Date(Date.now()).toLocaleString();
     var item = {name:_itemName, expiredate:_itemExpiration, creationdate:today};
@@ -19,11 +18,11 @@ buttonAdd.onclick = function() {
     expirationAdded.textContent = _itemExpiration;
     dateAdded.textContent = today;
 
-    var tableMain = document.querySelector('.tableMain');
+    var listBody = document.querySelector('#listBody');
 
     tableRow.appendChild(nameAdded);
     tableRow.appendChild(expirationAdded);
     tableRow.appendChild(dateAdded);
-    tableMain.appendChild(tableRow)
+    listBody.appendChild(tableRow)
 
 }
