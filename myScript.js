3016f8d5a29b38276537e3c1f7ae0d80f38b56a6
Mailys Terrier <terriems@mail.uc.edu>
2018-03-03 22:59:48 -0500

    Added new item to table and saved to items array

--- myScript.js
+++ myScript.js
@@ -7,15 +7,22 @@ if (typeof console == "undefined") {
     };
 }
 
-var items = [];
+var items = new Array();
+var table = document.getElementById("itemList")
 
 function SaveForm(form) {
     var _name = form.itemname.value;
     var _expiredate = form.expiredate.value;
     var today = new Date(Date.now()).toLocaleString();
-    var item = {name:_name, expiredate:form.expiredate.value, creationdate:today};
+    var item = {name:_name, expiredate:_expiredate, creationdate:today};
     items.push(item);
-    console.log(items);
+    var row = table.insertRow(0);
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    var cell3 = row.insertCell(2);
+    cell1.innerHTML = _name;
+    cell2.innerHTML = _expiredate;
+    cell3.innerHTML = today;
 }
 
 });
