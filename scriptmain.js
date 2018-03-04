efebe35f7b8e4b43a7d2f7b16d7330dde47a9934
patel3mh <patel3mh@mail.uc.edu>
2018-03-03 22:59:42 -0500

    add table variables

---
+++ scriptmain.js
@@ -0,0 +1,11 @@
+var name = document.querySelector(".name")
+var date_added = document.querySelector(".date_added")
+var expiration = document.querySelector(".expiration")
+var buttonAdd = document.querySelector(".addItem")
+
+buttonAdd.onclick = function() {
+  var item = name.value;
+
+}
+
+
